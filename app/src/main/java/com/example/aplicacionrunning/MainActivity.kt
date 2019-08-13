0f5866aeeb4fe9a27cb1207868c71c5244ac2266
Ramiro2709 <vargasramiro99@gmail.com>
2019-08-13 20:58:19 -0300

    a

--- app/src/main/java/com/example/aplicacionrunning/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionrunning/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
+//import androidx.fragment.app.FragmentActivity //NOTE importar
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,8 +13,19 @@ import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+
+
+//NOTE Se agrega el OnMapReadyCallBack para poder usar los mapas
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mFusedLocationClient: FusedLocationProviderClient? = null
     private var locationRequest: LocationRequest? = null
@@ -24,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     private var mLatitudeText: TextView? = null
     private var mLongitudeText: TextView? = null
 
+    private var mapFragment: SupportMapFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +56,25 @@ class MainActivity : AppCompatActivity() {
                 locationResult ?: return //?
                 for (location in locationResult.locations){
                     //NOTE !! Es para solo asignar cuando no es null(?
+                    mLastLocation = location;
                     mLongitudeText!!.text = location.toString();
                 }
+
+                //NOTE Agrega un marcador en la posicion actual
+                mapaRecibido!!.addMarker(
+                    MarkerOptions()
+                        //.position(LatLng(0.0, 0.0))
+                        .position(LatLng(mLastLocation!!.latitude, mLastLocation!!.longitude))
+                        .title("Marker")
+                )
             }
         }
-        askForPermissions();
+        askForPermissions()
 
+        //NOTE Asigna el fragment con el mapa
+            mapFragment =
+                supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+            mapFragment?.getMapAsync(this)
     }
 
     override fun onStart() {
@@ -81,29 +107,38 @@ class MainActivity : AppCompatActivity() {
         mFusedLocationClient!!.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */)
     }
 
-    //SECTION Pide Permisos
-    private fun askForPermissions() {
-        //NOTE Si no estan los permisos FINE o COARSE
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED)
-        {
-            //NOTE (?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION))
-            {
 
-            }
-            //NOTE Pregunta por permisos
-            else {
-
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 0
-                )
-            }
-        }
+    private var mapaRecibido: GoogleMap? = null
+    //NOTE Cuando recibe un mapa, lo asigna a una variable
+    override fun onMapReady(map: GoogleMap) {
+        mapaRecibido = map
     }
+
+//SECTION Pide Permisos
+private fun askForPermissions() {
+//NOTE Si no estan los permisos FINE o COARSE
+if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION
+    ) != PackageManager.PERMISSION_GRANTED ||
+    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION
+    ) != PackageManager.PERMISSION_GRANTED)
+{
+    //NOTE (?
+    if (ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION))
+    {
+
+    }
+    //NOTE Pregunta por permisos
+    else {
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 0
+        )
+    }
+}
+}
+
+
 }
